2791528c719fc6be2c54a1d6e32107b02b523d5e
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2026-02-05 22:49:27 -0300

    feat: registra o gerador EF Core DbContext no program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using rdi_rh_solution.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// registra o gerador EF Core DbContext 
+builder.Services.AddDbContext<RdiRhSolutionContext>();
 
 var app = builder.Build();
 
